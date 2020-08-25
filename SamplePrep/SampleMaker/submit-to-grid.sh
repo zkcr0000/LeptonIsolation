2794f64645bab286d134a065c7167f4972550de0
Cunwei Fan <fancunwei95@gmail.com>
2020-08-25 19:36:52 +0200

    hello

--- SamplePrep/SampleMaker/submit-to-grid.sh
+++ SamplePrep/SampleMaker/submit-to-grid.sh
@@ -9,9 +9,14 @@ source build/x86_64-centos7-gcc8-opt/setup.sh
 GRID_NAME=${RUCIO_ACCOUNT-${USER}}
 JOB_TAG=$(date +%F-%H-%M)
 
+#INPUT_DATASETS=(
+#   mc16_13TeV.361106.PowhegPythia8EvtGen_AZNLOCTEQ6L1_Zee.deriv.DAOD_SUSY2.e3601_e5984_s3126_r10724_r10726_p3895
+#   mc16_13TeV.308093.Sherpa_221_NNPDF30NNLO_Zmm2jets_Min_N_TChannel.deriv.DAOD_SUSY2.e5767_e5984_s3126_r10724_r10726_p3875
+#)
+
+
 INPUT_DATASETS=(
-    mc16_13TeV.361106.PowhegPythia8EvtGen_AZNLOCTEQ6L1_Zee.deriv.DAOD_SUSY2.e3601_e5984_s3126_r10724_r10726_p3895
-    mc16_13TeV.308093.Sherpa_221_NNPDF30NNLO_Zmm2jets_Min_N_TChannel.deriv.DAOD_SUSY2.e5767_e5984_s3126_r10724_r10726_p3875
+	
 )
 
 lsetup panda
